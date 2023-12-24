a6060d86635e4efb83dfd083c532165486b3c72a
Ashraf Muhamad <ashrafmuhamad2014@gmail.com>
2023-12-24 16:43:24 +0300

    fini la page tournée

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,28 @@
+dmx.config({
+  "main": {
+    "global": [
+      {
+        "type": "text",
+        "name": "title"
+      }
+    ]
+  },
+  "navbar": {
+    "localStorage": [
+      {
+        "type": "text",
+        "name": "nom"
+      },
+      {
+        "type": "text",
+        "name": "matricule"
+      }
+    ]
+  },
+  "sidebar": {
+    "actualPage": {
+      "meta": null,
+      "outputType": "text"
+    }
+  }
+});
